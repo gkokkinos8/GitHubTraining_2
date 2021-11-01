a8aca6637cf00da62373789ad65bdf66aef49c7a
gkokkinos <gkokkinos@profilesw.com>
2021-11-01 10:52:47 +0200

    Change again the Hello World Message

--- GitHubTraining_2/Program.cs
+++ GitHubTraining_2/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubTraining_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Giorgos, Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
